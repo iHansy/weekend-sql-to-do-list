a928dad36add777565aa010f0cea31e772cc95f8
Hans Accola <Hansolo6283@gmail.com>
2020-08-10 22:09:32 -0500

    background image covers, updated a few other things

--- database.sql
+++ database.sql
@@ -11,22 +11,3 @@ VALUES('Clean room', 'Vacuum, sort clothes, and organize.', '8-10-2020', 'false'
 
 INSERT INTO "tasks" ("task", "description", "due_date")
 VALUES('Sell headphones', 'Sanitize, charge, then post on FB marketplace.', '8-13-2020');
-
-INSERT INTO "tasks" ("task")
-VALUES('Go running');
-
-INSERT INTO "tasks" ("task")
-VALUES('Go running');
-
-SELECT * FROM "tasks" ORDER BY "due_date";
-
-SELECT to_char("due_date", 'DD/MM/YYYY') FROM "tasks"; -- didn't work
-SELECT * FROM "tasks";
-
-DROP TABLE "tasks";
-
-UPDATE "tasks" -- didn't work
-SET "due_date" = 'to_char("due_date", 'DD/MM/YYYY'; 
-
-DELETE FROM "tasks"
-WHERE "id" = 7;
