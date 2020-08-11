76a0607e2e2696b7f652a603e9cd26457e35b9cf
Hans Accola <Hansolo6283@gmail.com>
2020-08-11 00:13:12 -0500

    sweet herou deployment

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -2,14 +2,36 @@ const express = require('express');
 const taskRouter = express.Router();
 const pg = require('pg');
 
-// SQL database connection
-const config = {
-    database: 'weekend-to-do-app',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 10000
+let config = {};
+
+if (process.env.DATABASE_URL) {
+
+    // heroku gives a url, not a connection object
+    const params = url.parse(process.env.DATABASE_URL);
+    const auth = params.auth.split(':');
+
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: true, // heroku requires ssl to be true
+        max: 10, // max number of clients in the pool
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before close
+    };
+} else {
+    // SQL local database connection
+    config = {
+        database: 'weekend-to-do-app',
+        host: 'localhost',
+        port: 5432,
+        max: 10,
+        idleTimeoutMillis: 10000
+    };
 }
+
+
 
 const pool = new pg.Pool(config);
 // connecting to postgresql
